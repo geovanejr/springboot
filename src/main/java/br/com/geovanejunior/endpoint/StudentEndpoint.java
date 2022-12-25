78a8d6f55703ad9a2a1e6afd0cf0b39c3eeb0f45
Geovane Junior <geovane.gjunior@gmail.com>
2022-12-25 20:34:21 -0300

    Spring Boot Essentials 02 - @Component, @Autowired e @SpringBootApplication

--- src/main/java/br/com/geovanejunior/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/geovanejunior/endpoint/StudentEndpoint.java
@@ -1,10 +1,13 @@
-package endpoint;
+package br.com.geovanejunior.endpoint;
 
-import model.Student;
+import br.com.geovanejunior.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.geovanejunior.util.DateUtil;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -13,9 +16,13 @@ import static java.util.Arrays.asList;
 @RequestMapping("student")
 public class StudentEndpoint {
 
+    @Autowired
+    private DateUtil dateUtil;
+
     @GetMapping(path = "/list")
     public List<Student> listAll() {
 
+        System.out.println("Data: " + dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return asList(new Student("Geovane"), new Student("Lilian"));
     }
 }
--- src/main/java/br/com/geovanejunior/model/Student.java
+++ src/main/java/br/com/geovanejunior/model/Student.java
@@ -1,4 +1,4 @@
-package model;
+package br.com.geovanejunior.model;
 
 public class Student {
     private String name;
